8051885f02db9e7bde3cce380f22de4a46ac9145
Marco Boom <marco@boomstruik.nl>
2019-09-02 14:19:16 +0200

    Accept birth_date as additional_date in Klarna

--- Model/Paymentmethod/Klarna.php
+++ Model/Paymentmethod/Klarna.php
@@ -9,4 +9,20 @@ class Klarna extends PaymentMethod
     {
         return 1717;
     }
+
+    public function assignData(\Magento\Framework\DataObject $data)
+    {
+        parent::assignData($data);
+
+        if (is_array($data) && array_key_exists('birth_date', $data)) {
+            $this->getInfoInstance()->setAdditionalInformation('birth_date', $data['birth_date']);
+        } elseif ($data instanceof \Magento\Framework\DataObject) {
+            $additional_data = $data->getAdditionalData();
+            if (isset($additional_data['birth_date'])) {
+                $birthDate = $additional_data['birth_date'];
+                $this->getInfoInstance()->setAdditionalInformation('birth_date', $birthDate);
+            }
+        }
+        return $this;
+    }
 }
